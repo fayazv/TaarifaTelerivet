17a288997029f43ebcb593c8a32136b4cb430402
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-08-16 15:40:55 +0100

    Proper formatting

--- TaarifaTelerivet.js
+++ TaarifaTelerivet.js
@@ -1,10 +1,26 @@
-var parts = content.split(/#/g);  //splits the message into multiple parts, i.e. structured text messages
-  if (parts.length != 2) //if the message does not come in two parts send out a new message, otherwise procede to the webhook
-  {sendReply("Invalid message format\n."+"kama kituo cha maji hakifanyi kazi\n"+"Please use name#status\n"+"jina la kituo cha maji#not functional");
-  return;} //the "\n" simply ends the current line
-  sendReply("Your report has been recorded!\n" + "Asante kwa ujumbe wako" + "wp/kituo cha maji: " + parts[0] + "\n" + "Num Status/hadhi: " + parts[1] + "\n");
-var response = httpClient.request("http://iringa.herokuapp.com/api/requests", { //information from text gets sent to this webaddress
-  method: "POST", //information is posted, other methods include get
-  data: JSON.stringify({"service_code": "wps001","attribute": {"waterpoint_id": parts[0], "status": parts[1] }}),
-  headers: {'Content-Type': 'application/json'}});
-console.log(response.content); //logs in the Telerivert console, line not really necessary
+//splits the message into multiple parts, i.e. structured text messages
+var parts = content.split(/#/g);
+
+//if the message does not come in two parts send out a new message, otherwise procede to the webhook
+if (parts.length != 2) {
+  sendReply("Invalid message format\n." + "kama kituo cha maji hakifanyi kazi\n" + "Please use name#status\n" + "jina la kituo cha maji#not functional");
+  return;
+}
+
+sendReply("Your report has been recorded!\n" + "Asante kwa ujumbe wako" + "wp/kituo cha maji: " + parts[0] + "\n" + "Num Status/hadhi: " + parts[1] + "\n");
+
+var response = httpClient.request("https://iringa.herokuapp.com/api/requests", {
+  method: "POST",
+  data: JSON.stringify({
+    "service_code": "wps001",
+    "attribute": {
+      "waterpoint_id": parts[0],
+      "status": parts[1]
+    }
+  }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+// Log to the Telerivert console
+console.log(response.content);
