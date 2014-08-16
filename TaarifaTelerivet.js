f650ef128935de06b818ab7917466411e886684e
Dirk Gorissen <dgorissen@gmail.com>
2014-08-16 14:54:27 +0100

    Change webhook url to the iringa instance

--- TaarifaTelerivet.js
+++ TaarifaTelerivet.js
@@ -3,7 +3,7 @@ var parts = content.split(/#/g);  //splits the message into multiple parts, i.e.
   {sendReply("Invalid message format\n."+"kama kituo cha maji hakifanyi kazi\n"+"Please use name#status\n"+"jina la kituo cha maji#not functional");
   return;} //the "\n" simply ends the current line
   sendReply("Your report has been recorded!\n" + "Asante kwa ujumbe wako" + "wp/kituo cha maji: " + parts[0] + "\n" + "Num Status/hadhi: " + parts[1] + "\n");
-var response = httpClient.request("http://570cd4ea.ngrok.com/api/requests", { //information from text gets send to this webaddress
+var response = httpClient.request("http://iringa.herokuapp.com/api/requests", { //information from text gets sent to this webaddress
   method: "POST", //information is posted, other methods include get
   data: JSON.stringify({"service_code": "wps001","attribute": {"waterpoint_id": parts[0], "status": parts[1] }}),
   headers: {'Content-Type': 'application/json'}});
